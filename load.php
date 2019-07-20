1593f4b54a024f9e4969ee526daf07c19ad6542c
Fahmi Syaifudin <fahmisyaifudin00@gmail.com>
2019-07-20 07:31:41 +0700

    Penambahan keterangn

--- load.php
+++ load.php
@@ -4,6 +4,7 @@
       <th scope="col">Waktu</th>
       <th scope="col">Suhu</th>
       <th scope="col">Kelembapan</th>
+      <th scope="col">Keterangan</th>
     </tr>
   </thead>
   <tbody>
@@ -12,10 +13,18 @@
 include 'koneksi.php';
 $query = mysqli_query($koneksi, "SELECT * FROM data" );
                                     while ($row = mysqli_fetch_array($query)) {
+                                    	if ($row['kelembapan'] < 15) {
+                                    		$status = 'Lampu Nyala';
+                                    	}else if ($row['kelembapan'] > 40) {
+                                    		$status = 'Kipas Nyala';
+                                    	}else{
+                                    		$status = 'Kipas dan Lampu Mati';
+                                    	}
                                 echo         '<tr>
                                                 <th scope="row">'.$row['waktu'].'</th>
                                                 <td>'.$row['suhu'].'</td>
                                                 <td>'.$row['kelembapan'].'</td>
+                                                <td>'.$status.'</td>
                                                 </tr>';
                                     }
                                     ?>
